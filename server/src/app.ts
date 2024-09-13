a909508fca48dcac5276cfe1b6360f0aaf6ccc52
Pablo Alan <pabloxt14@gmail.com>
2024-09-13 00:18:00 -0300

    feat: config cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,8 +3,13 @@ import { ZodError } from 'zod'
 import { env } from './env'
 import { goalsRoutes } from './http/controllers/goals/routes'
 import { summaryRoutes } from './http/controllers/summary/routes'
+import fastifyCors from '@fastify/cors'
 
 export const app = fastify()
+
+app.register(fastifyCors, {
+  origin: env.NODE_ENV === 'production' ? env.FRONTEND_URL : '*',
+})
 
 app.register(goalsRoutes)
 app.register(summaryRoutes)
