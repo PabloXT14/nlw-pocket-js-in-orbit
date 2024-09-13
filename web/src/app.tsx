20643e5985aeafa59b0142577bc11b660d6675d9
Pablo Alan <pabloxt14@gmail.com>
2024-09-13 20:52:07 -0300

    feat: create empty goals page and add ui components

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,3 +1,23 @@
+import logoInOrbit from './assets/logo-in-orbit.svg'
+import letsStartIllustration from './assets/lets-start-illustration.svg'
+import { Plus } from 'lucide-react'
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <h1 className="text-3xl text-center my-4">Hello</h1>
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logoInOrbit} alt="logo in orbit" />
+      <img src={letsStartIllustration} alt="lets start illustration" />
+
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
+        mesmo?
+      </p>
+
+      <Button>
+        <Plus className="size-4" />
+        Cadastrar meta
+      </Button>
+    </div>
+  )
 }
