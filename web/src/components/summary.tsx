f73b8cda3d0ba834bd5f041e2b9781beefded68a
Pablo Alan <pabloxt14@gmail.com>
2024-09-15 19:35:41 -0300

    refactor: summary goalsPerDay error

--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -4,7 +4,6 @@ import { DialogTrigger } from './ui/dialog'
 import { InOrbitIcon } from './in-orbit-icon'
 import { Progress, ProgressIndicator } from './ui/progress-bar'
 import { Separator } from './ui/separator'
-import { OutlineButton } from './ui/outline-button'
 import { useQuery } from '@tanstack/react-query'
 import { getSummary } from '../http/get-summary'
 import dayjs from 'dayjs'
@@ -17,6 +16,8 @@ export const Summary = () => {
     staleTime: 1000 * 60, // 60 seconds
   })
 
+  console.log(summary)
+
   if (!summary) {
     return null
   }
@@ -25,7 +26,7 @@ export const Summary = () => {
   const lastDayOfWeek = dayjs().endOf('week').format('D MMM')
 
   const completedPercentage = Math.round(
-    (summary.completed / summary.total) * 100
+    (summary?.completed / summary?.total) * 100
   )
 
   return (
@@ -77,36 +78,42 @@ export const Summary = () => {
         <h2 className="text-xl font-medium">Sua semana</h2>
 
         {/* Goal By Week Day */}
-        {Object.entries(summary.goalsPerDay).map(([date, goals]) => {
-          const weekDay = dayjs(date).format('dddd')
-          const dayAndMonth = dayjs(date).format('DD[ de ]MMMM')
+        {summary.goalsPerDay ? (
+          Object.entries(summary.goalsPerDay).map(([date, goals]) => {
+            const weekDay = dayjs(date).format('dddd')
+            const dayAndMonth = dayjs(date).format('DD[ de ]MMMM')
 
-          return (
-            <div key={date} className="flex flex-col gap-4">
-              <h3 className="font-medium">
-                <span className="capitalize">{weekDay}</span>{' '}
-                <span className="text-zinc-400 text-xs">({dayAndMonth})</span>
-              </h3>
+            return (
+              <div key={date} className="flex flex-col gap-4">
+                <h3 className="font-medium">
+                  <span className="capitalize">{weekDay}</span>{' '}
+                  <span className="text-zinc-400 text-xs">({dayAndMonth})</span>
+                </h3>
 
-              <ul className="flex flex-col gap-3">
-                {goals.map(goal => {
-                  const time = dayjs(goal.completedAt).format('HH:mm')
+                <ul className="flex flex-col gap-3">
+                  {goals.map(goal => {
+                    const time = dayjs(goal.completedAt).format('HH:mm')
 
-                  return (
-                    <li key={goal.id} className="flex items-center gap-2">
-                      <CheckCircle2 className="size-4 text-pink-500" />
-                      <span className="text-sm text-zinc-400">
-                        Você completou "
-                        <span className="text-zinc-100">{goal.title}</span>" às{' '}
-                        <span className="text-zinc-100">{time}h</span>
-                      </span>
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
-          )
-        })}
+                    return (
+                      <li key={goal.id} className="flex items-center gap-2">
+                        <CheckCircle2 className="size-4 text-pink-500" />
+                        <span className="text-sm text-zinc-400">
+                          Você completou "
+                          <span className="text-zinc-100">{goal.title}</span>"
+                          às <span className="text-zinc-100">{time}h</span>
+                        </span>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+            )
+          })
+        ) : (
+          <p className="text-sm text-zinc-400">
+            Você ainda não completou nenhuma meta essa semana.
+          </p>
+        )}
       </div>
     </div>
   )
