3377800116620e6880505b27c64fcb5825602352
Pablo Alan <pabloxt14@gmail.com>
2024-09-14 22:53:17 -0300

    feat: implement create goal logic

--- web/src/components/create-goal.tsx
+++ web/src/components/create-goal.tsx
@@ -13,6 +13,11 @@ import {
   RadioGroupItem,
 } from './ui/radio-group'
 import { Button } from './ui/button'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useQueryClient } from '@tanstack/react-query'
+import { createGoal } from '../http/create-goal'
 
 const FREQUENCY_OPTIONS = [
   { value: '1', label: '1x na semana', icon: '🥱' },
@@ -24,7 +29,37 @@ const FREQUENCY_OPTIONS = [
   { value: '7', label: 'Todos dias da semana', icon: '🔥' },
 ]
 
+const createGoalFormSchema = z.object({
+  title: z.string().min(1, 'Informe o nome da meta que deseja realizar'),
+  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+})
+
+type CreateGoalFormData = z.infer<typeof createGoalFormSchema>
+
 export const CreateGoal = () => {
+  const queryClient = useQueryClient()
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<CreateGoalFormData>({
+    resolver: zodResolver(createGoalFormSchema),
+  })
+
+  async function handleCreateGoal(data: CreateGoalFormData) {
+    const { title, desiredWeeklyFrequency } = data
+
+    await createGoal({ title, desiredWeeklyFrequency })
+
+    reset()
+
+    queryClient.invalidateQueries({ queryKey: ['summary'] })
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+  }
+
   return (
     <DialogContent>
       <div className="flex flex-col gap-6 h-full">
@@ -44,33 +79,54 @@ export const CreateGoal = () => {
         </div>
 
         {/* Form */}
-        <form className="flex flex-1 flex-col justify-between">
+        <form
+          onSubmit={handleSubmit(handleCreateGoal)}
+          className="flex flex-1 flex-col justify-between"
+        >
           {/* Top */}
           <div className="flex flex-col gap-6">
             {/* Activity Input */}
             <div className="flex flex-col gap-2">
               <Label htmlFor="title">Qual a atividade?</Label>
               <Input
+                {...register('title')}
                 id="title"
                 autoFocus
                 placeholder="Praticar exercícios, meditar, etc ..."
               />
+              {errors.title && (
+                <p className="text-red-400 text-sm">{errors.title.message}</p>
+              )}
             </div>
 
             {/* Frequency Input */}
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 overflow-y-auto">
               <Label>Quantas vezes na semana?</Label>
-              <RadioGroup>
-                {FREQUENCY_OPTIONS.map(option => (
-                  <RadioGroupItem key={option.value} value={option.value}>
-                    <RadioGroupIndicator />
-                    <span className="text-zinc-300 text-sm font-medium leading-none">
-                      {option.label}
-                    </span>
-                    <span className="text-lg leading-none">{option.icon}</span>
-                  </RadioGroupItem>
-                ))}
-              </RadioGroup>
+              <Controller
+                control={control}
+                name="desiredWeeklyFrequency"
+                defaultValue={Number(FREQUENCY_OPTIONS[0].value)}
+                render={({ field }) => {
+                  return (
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={String(field.value)}
+                    >
+                      {FREQUENCY_OPTIONS.map(option => (
+                        <RadioGroupItem key={option.value} value={option.value}>
+                          <RadioGroupIndicator />
+                          <span className="text-zinc-300 text-sm font-medium leading-none">
+                            {option.label}
+                          </span>
+                          <span className="text-lg leading-none">
+                            {option.icon}
+                          </span>
+                        </RadioGroupItem>
+                      ))}
+                    </RadioGroup>
+                  )
+                }}
+              />
             </div>
           </div>
 
